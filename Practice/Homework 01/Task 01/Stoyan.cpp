dfe593087f282c3ac98b10ab6feffc2ee0d2b4f9
Stoyan Zlatev <47269030+Tony5768@users.noreply.github.com>
2022-10-09 17:33:03 +0300

    Create Stoyan.cpp

---
+++ Practice/Homework 01/Task 01/Stoyan.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+    string word;
+    cin >> word;
+
+    char variant;
+    cin >> variant;
+
+    vector<int> indexes;
+    for (size_t i = 0; i < word.size(); ++i) {
+        if (word[i] == variant) {
+            indexes.push_back(i);
+        }
+    }
+
+    size_t minDistance;
+    for (int i = 0; i < word.size(); i++) {
+        minDistance = INTMAX_MAX;
+        for (int index: indexes) {
+            minDistance = (abs(index - i) < minDistance) ? abs(index - i): minDistance;
+        }
+        cout << minDistance << " ";
+    }
+
+    return 0;
+}
