b7213a2e6779884b1b2ebd824a30f4f6f126c974
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2022-10-10 20:02:54 +0300

    Add Practice P01 Task03

---
+++ Practice/Practicum/Practicum 01/Task 03/Tsvetilin.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+const size_t MAX_SIZE = 1e5;
+int walls[MAX_SIZE]{};
+size_t max[MAX_SIZE]{};
+
+int main()
+{
+    size_t n;
+
+    std::cin >> n;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cin >> walls[i];
+        size_t maxVol = 0;
+        for (size_t j = 0; j < i; j++)
+        {
+            size_t vol = std::min(walls[j], walls[i]) * (i - j);
+            if (vol > maxVol)
+            {
+                maxVol = vol;
+            }
+        }
+
+        max[i] = maxVol;
+    }
+
+    size_t maxVol = max[0];
+    for (size_t i = 0; i < n; i++)
+    {
+        if (maxVol < max[i])
+        {
+            maxVol = max[i];
+        }
+    }
+
+    std::cout<<maxVol;
+}
