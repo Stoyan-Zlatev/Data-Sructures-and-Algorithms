ae56f7f330242b80d7aea148384bd0a8e7acffe7
Stoyan Zlatev <47269030+Tony5768@users.noreply.github.com>
2022-10-19 21:09:38 +0300

    Refactor

--- Algorithms/Searching/TernarySearch/Stoyan.cpp
+++ Algorithms/Searching/TernarySearch/Stoyan.cpp
@@ -16,7 +16,7 @@ int ternarySearch(const T *arr, int start, int end, const T &searched) {
     if (arr[mid1] > searched) {
         return ternarySearch(arr, start, mid1 - 1, searched);
     } else if (arr[mid2] > searched) {
-        return ternarySearch(arr, mid1 + 1, mid2 - 2, searched);
+        return ternarySearch(arr, mid1 + 1, mid2 - 1, searched);
     }
 
     return ternarySearch(arr, mid2 +1, end, searched);
