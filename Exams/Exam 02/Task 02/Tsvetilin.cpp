9d61d5066a7bdd98f7c596975ef8b33841b3d756
Tsvetilin Tsvetilov <tsvetilin.tsvetilov@gmail.com>
2022-11-10 23:10:36 +0200

    Exam 02 Task 02 Solution

---
+++ Exams/Exam 02/Task 02/Tsvetilin.cpp
@@ -0,0 +1,247 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+struct Node
+{
+    int value;
+    Node *next;
+    // Constructor with two arguments, one of which is default
+    Node(int value, Node *next = nullptr) : value(value), next(next){};
+};
+
+struct SinglyLinkedList
+{
+    Node *head, *tail;
+    int size;
+
+    SinglyLinkedList() : head(nullptr), tail(nullptr), size(0){}; // Default constructor
+
+    void addAtHead(int X); // Utility function for add
+    void addAtTail(int X); // Utility function for add
+    void add(int X, int pos);
+
+    void remove(int pos);
+
+    void reverse(); // Iterative
+
+    Node *reverseRecursiveUtil(Node *head); // Utility function for recursive reverse function
+    void reverseRecursive();                // Recursive
+
+    bool isPalindrom();
+
+    int count(int X);
+
+    void replace(int X);
+
+    void group(int start, int end);
+
+    // Instead of print function
+    friend std::ostream &operator<<(std::ostream &os, const SinglyLinkedList &list);
+};
+
+int main()
+{
+    size_t N, Q;
+    std::cin >> N;
+
+    int el(0), pos(0);
+
+    for (size_t i = 0; i < N; i++)
+    {
+        SinglyLinkedList list;
+        std::cin >> Q;
+        std::cin.ignore();
+        for (size_t i = 0; i < Q; i++)
+        {
+
+            std::string line;
+            getline(std::cin, line);
+            std::istringstream istr(line);
+
+            std::string command;
+            istr >> command;
+
+            if (command == "add")
+            {
+                istr >> el >> pos;
+                list.add(el, pos);
+            }
+            else if (command == "print")
+            {
+                std::cout << list;
+            }
+            else if (command == "remove")
+            {
+                istr >> pos;
+                list.remove(pos);
+            }
+            else if (command == "replace")
+            {
+                istr >> el;
+                list.replace(el);
+                std::cout << list << "\n";
+            }
+            else
+            {
+                int count(0);
+                while (istr)
+                {
+                    istr >> command;
+                    count++;
+                }
+                i += count - 1;
+            }
+        }
+    }
+    return 0;
+}
+
+std::ostream &operator<<(std::ostream &os, const SinglyLinkedList &list)
+{
+    Node *traverse = list.head;
+    while (traverse != nullptr)
+    {
+        os << traverse->value << '#';
+        traverse = traverse->next;
+    } // os << '\n';
+    return os;
+}
+
+void SinglyLinkedList::addAtHead(int X)
+{
+    Node *newNode = new Node(X);
+    if (head == nullptr) // tail will then also point to nullptr
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        Node *temp = head;
+        newNode->next = temp;
+        head = newNode;
+    }
+    size++;
+}
+
+void SinglyLinkedList::addAtTail(int X)
+{
+    Node *newNode = new Node(X);
+    if (head == nullptr)
+    {
+        head = tail = newNode;
+    }
+    else
+    {
+        tail->next = newNode;
+        tail = newNode;
+    }
+    size++;
+}
+
+void SinglyLinkedList::add(int X, int pos)
+{
+    if (pos == 0)
+    {
+        addAtHead(X);
+    }
+    else if (pos < 0 || pos > size)
+    {
+        addAtTail(X);
+        // std::cout << "add_last";
+    }
+    else if (pos == size)
+    {
+        addAtTail(X);
+    }
+    else
+    {
+        Node *newNode = new Node(X);
+        Node *traverse = head;
+        for (int i = 1; i < pos; i++)
+        {
+            traverse = traverse->next;
+        }                            // traverse points to (pos-1)th Node
+        Node *temp = traverse->next; // pos-th Node  :  traverse - newNode - temp
+        traverse->next = newNode;
+        newNode->next = temp;
+        size++;
+    }
+}
+
+void SinglyLinkedList::remove(int pos)
+{
+    if (pos < 0 || pos >= (int)size)
+    {
+        std::cout << "remove_failed";
+    }
+    else if (pos == 0)
+    {
+        Node *temp = head->next;
+        delete head;
+        head = temp;
+        size--;
+    }
+    else
+    {
+        Node *traverse = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            traverse = traverse->next;
+        }                            // traverseNode points to (pos-1)th Node
+        Node *temp = traverse->next; // pos-th Node (node we want to remove)
+        traverse->next = temp->next; // (pos+1)th Node
+        if (temp->next == nullptr)
+        {
+            tail = traverse;
+        }
+        delete temp; // remove temp;
+        size--;
+    }
+}
+
+
+// Exam task:
+void SinglyLinkedList::replace(int X)
+{
+    Node *current = head;
+    Node *prev = nullptr;
+    int count = 0;
+
+    while (count < X && current)
+    {
+        current = current->next;
+        ++count;
+    }
+
+    if (current == nullptr)
+    {
+        return;
+    }
+
+    int value = current->value;
+    prev = head;
+    current = head->next;
+
+    while (current)
+    {
+        if (!prev || !current)
+        {
+            return;
+        }
+
+        if (prev->value == value)
+        {
+            prev->value = current->value;
+        }
+
+        prev = current;
+        current = current->next;
+    }
+}
