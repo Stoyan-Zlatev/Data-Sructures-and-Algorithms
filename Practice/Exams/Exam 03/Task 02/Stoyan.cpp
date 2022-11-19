a0a6c5cf55bc0575759214e421b6a1d752e14004
Stoyan Zlatev <47269030+Tony5768@users.noreply.github.com>
2022-11-19 14:52:26 +0200

    Add E3 Task 02 Solution

---
+++ Practice/Exams/Exam 03/Task 02/Stoyan.cpp
@@ -0,0 +1,78 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+struct Node {
+    Node *left;
+    Node *right;
+    int value;
+
+    Node(int value) {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+class BST {
+
+public:
+    BST() {
+        root = NULL;
+    }
+
+    void insert(int value) {
+        root = insert(root, value);
+    }
+
+    void printSpecific(int dividedBy) {
+        printNoLeaves(root, dividedBy);
+    }
+
+private:
+    void printNoLeaves(Node *root, int dividedBy) {
+        if (root == NULL)
+            return;
+
+        if (root->value % dividedBy == 0 && (root->left != NULL || root->right != NULL)) {
+            cout << root->value << " ";
+        }
+        printNoLeaves(root->left, dividedBy);
+        printNoLeaves(root->right, dividedBy);
+    }
+
+
+    Node *root;
+
+    Node *insert(Node *curNode, int value) {
+        if (curNode == NULL) {
+            curNode = new Node(value);
+        } else if (curNode->value < value) {
+            curNode->right = insert(curNode->right, value);
+        } else if (curNode->value > value) {
+            curNode->left = insert(curNode->left, value);
+        } else {
+            //if we already have this value at the tree - do nothing
+        }
+        return curNode;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    int value;
+    BST tree;
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        tree.insert(value);
+    }
+    int dividedBy;
+    cin >> dividedBy;
+    tree.printSpecific(dividedBy);
+    return 0;
+}
