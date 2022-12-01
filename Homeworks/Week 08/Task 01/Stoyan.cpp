d44c4d0604f96b09ad5e986bf015ead251208cee
Stoyan Zlatev <47269030+Tony5768@users.noreply.github.com>
2022-12-02 00:35:47 +0200

    Add HW 08 Task 01 Solution

--- Homeworks/Week 08/Task 01/Stoyan.cpp
+++ Homeworks/Week 08/Task 01/Stoyan.cpp
@@ -2,33 +2,60 @@
 
 using namespace std;
 
-priority_queue<size_t, vector<size_t>, greater<size_t>> numbers;
+struct Time {
+    size_t startTime;
+    size_t neededTime;
+    size_t index;
+    
+    Time(size_t startTime, size_t neededTime, size_t index) {
+        this->startTime = startTime;
+        this->neededTime = neededTime;
+        this->index = index;
+    }
+
+    bool operator>(const Time &other) const {
+        return this->startTime > other.startTime;
+    }
+
+    bool operator<(const Time &other) const {
+        return this->neededTime > other.neededTime ||
+               (this->neededTime == other.neededTime && this->index > other.index);
+    }
+
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    size_t K, T, B, N, number;
-    cin >> K >> T >> B >> N;
+    size_t N, t, f;
+    cin >> N;
 
-    for (size_t i = 1; i <= N && B > 0; i++) {
-        cin >> number;
-        numbers.push(number);
-        if (i % T == 0) {
-            for (size_t j = 0; j < K; j++) {
-                cout << numbers.top() << " ";
-                numbers.pop();
-            }
-            B--;
-        }
+    priority_queue<Time, vector<Time>, greater<Time>> banici;
+    priority_queue<Time, vector<Time>, less<Time>> redBanici;
+
+    for (size_t i = 0; i < N; ++i) {
+        cin >> t >> f;
+        banici.push(Time(t, f, i));
     }
 
-    for (size_t i = 0; i < B; i++){
-        for (size_t j = 0; j < K; j++) {
-            cout << numbers.top() << " ";
-            numbers.pop();
+    size_t startTime;
+    for (size_t i = 0; i < N; ++i) {
+        if (redBanici.empty()) {
+            startTime = banici.top().startTime;
+            redBanici.push(banici.top());
+            banici.pop();
+        }
+        
+        cout << redBanici.top().index << " ";
+        startTime += redBanici.top().neededTime;
+        redBanici.pop();
+
+        while (!banici.empty() && banici.top().startTime <= startTime) {
+            redBanici.push(banici.top());
+            banici.pop();
         }
     }
-
+    
     return 0;
 }
