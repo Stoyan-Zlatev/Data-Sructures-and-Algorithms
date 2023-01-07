337389c96cafaae8b7437602910d8e0034483020
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-01-07 14:43:17 +0200

    Update Kruskal Algorithm

--- Algorithms/MST/Kruskal/Stoyan.cpp
+++ Algorithms/MST/Kruskal/Stoyan.cpp
@@ -18,27 +18,37 @@ void connect(size_t from, size_t to, size_t *parent) {
     parent[from] = parent[to];
 }
 
-void kruskal(size_t *parent) {
+size_t kruskal(size_t *parent) {
     sort(graph.begin(), graph.end());
+    size_t totalWeight = 0;
     for (size_t i = 0; i < graph.size(); ++i) {
         size_t u = find(graph[i].second.first, parent);
         size_t v = find(graph[i].second.second, parent);
 
         if (u != v) {
+            totalWeight += graph[i].first;
             tree.push_back(graph[i]);
             connect(u, v, parent);
         }
     }
+    
+    return totalWeight;
 }
 
 
+
 int main() {
-    size_t N, M;
+    size_t N, M, from, to, weight;
     cin >> N >> M;
     size_t parent[N];
+    for (int i = 0; i < M; ++i) {
+        cin >> from >> to >> weight;
+        graph.push_back({weight, {from, to}});
+    }
+
     for (int i = 0; i <= N; ++i) {
         parent[i] = i;
     }
-    //init graph
-    kruskal(parent);
+
+    cout << kruskal(parent);
 }
