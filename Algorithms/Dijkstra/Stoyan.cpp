a59deb4cbf989f50a9e7a1e783b33a169dcf4302
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-01-07 15:14:13 +0200

    Update Dijkstra Algorithm

--- Algorithms/Dijkstra/Stoyan.cpp
+++ Algorithms/Dijkstra/Stoyan.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 struct Node {
-    size_t to;
+    size_t destination;
     size_t weight;
 
     bool operator>(const Node &other) const {
@@ -13,53 +13,66 @@ struct Node {
 
 unordered_map<size_t, vector<Node>> graph;
 
-void dijkstra(const size_t &startNode, size_t *nodeDistances) {
-    nodeDistances[startNode] = 0;
+void findShortestPath(const size_t &startNode, size_t *distances) {
+    distances[startNode] = 0;
 
     priority_queue<Node, vector<Node>, greater<Node>> pq;
     pq.push({startNode, 0});
 
     while (!pq.empty()) {
-        size_t currentNode = pq.top().to;
-        size_t currentWeight = pq.top().weight;
+        size_t currentNode = pq.top().destination;
+        size_t distance = pq.top().weight;
         pq.pop();
 
-        if (currentWeight > nodeDistances[currentNode]) {
+        if (distance > distances[currentNode]) {
             continue;
         }
 
         for (const auto &neighbour: graph[currentNode]) {
-            size_t nextNode = neighbour.to;
-            size_t nextWeight = neighbour.weight;
-            size_t distanceToNextNode = currentWeight + nextWeight;
+            size_t nextNode = neighbour.destination;
+            size_t distanceToNextStop = neighbour.weight;
+            size_t totalDistance = distances[currentNode] + distanceToNextStop;
 
-            if (nodeDistances[nextNode] > distanceToNextNode) {
-                nodeDistances[nextNode] = distanceToNextNode;
-                pq.push({nextNode, distanceToNextNode});
+            if (distances[nextNode] > totalDistance) {
+                distances[nextNode] = totalDistance;
+                pq.push({nextNode, totalDistance});
             }
         }
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    size_t T, N, M, from, to, distance, start;
+    cin >> T;
 
-    size_t V, E, start, stop, A, B, dist;
-    cin >> V >> E >> start >> stop;
-    size_t nodeDistances[V];
+    for (int j = 0; j < T; ++j) {
+        cin >> N >> M;
 
-    for (size_t i = 0; i < E; ++i) {
-        cin >> A >> B >> dist;
-        graph[A].push_back({B, dist});
+        for (size_t i = 0; i < M; ++i) {
+            cin >> from >> to >> distance;
+            graph[from].push_back({to, distance});
+            graph[to].push_back({from, distance});
+        }
+
+        size_t distances[N + 1];
+        for (size_t i = 0; i <= N; ++i) {
+            distances[i] = INT_MAX;
+        }
+
+        cin >> start;
+
+        findShortestPath(start, distances);
+
+        for (size_t i = 1; i <= N; ++i) {
+            if (i != start) {
+                if (distances[i] != INT_MAX) {
+                    cout << distances[i] << " ";
+                } else {
+                    cout << -1 << " ";
+                }
+            }
+        }
+        graph.clear();
     }
 
-    dijkstra(start, nodeDistances);
-    if (nodeDistances[stop] != INT_MAX) {
-        cout << nodeDistances[stop];
-    } else {
-        cout << -1;
-    }
-
-    return 0;
 }
