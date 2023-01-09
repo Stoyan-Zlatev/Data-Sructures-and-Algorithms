14ec136efd0734931a4f95204cf15463dad185e0
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-01-09 11:40:49 +0200

    Update Kruskal Algorithm

--- Algorithms/MST/Kruskal/Stoyan.cpp
+++ Algorithms/MST/Kruskal/Stoyan.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
-vector<pair<size_t, pair<size_t, size_t>>> graph;
-vector<pair<size_t, pair<size_t, size_t>>> tree;
+struct Edge {
+    size_t from, to, weight;
+
+    bool operator<(const Edge &other) const {
+        return this->weight < other.weight;
+    }
+};
+
+vector<Edge> graph;
+vector<Edge> tree;
 
 size_t find(size_t v, size_t *parent) {
     if (v == parent[v]) {
@@ -22,31 +30,30 @@ size_t kruskal(size_t *parent) {
     sort(graph.begin(), graph.end());
     size_t totalWeight = 0;
     for (size_t i = 0; i < graph.size(); ++i) {
-        size_t u = find(graph[i].second.first, parent);
-        size_t v = find(graph[i].second.second, parent);
+        size_t u = find(graph[i].from, parent);
+        size_t v = find(graph[i].to, parent);
 
         if (u != v) {
-            totalWeight += graph[i].first;
+            totalWeight += graph[i].weight;
             tree.push_back(graph[i]);
             connect(u, v, parent);
         }
     }
-    
+
     return totalWeight;
 }
-
 
 
 int main() {
     size_t N, M, from, to, weight;
     cin >> N >> M;
     size_t parent[N];
-    for (int i = 0; i < M; ++i) {
+    for (size_t i = 0; i < M; ++i) {
         cin >> from >> to >> weight;
-        graph.push_back({weight, {from, to}});
+        graph.push_back({from, to, weight});
     }
 
-    for (int i = 0; i <= N; ++i) {
+    for (size_t i = 0; i <= N; ++i) {
         parent[i] = i;
     }
 
