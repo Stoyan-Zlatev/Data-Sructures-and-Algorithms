16ab0dfe2f66ea125353d1de65bf2e00b2ef25a3
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-01-15 01:19:35 +0200

    Add HW 13 Task 03 Solution

---
+++ Homeworks/Week 13/Task 03/Stoyan.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<char, size_t> wordMap;
+
+bool isPossible(const unordered_map<char, size_t> &tempWordMap) {
+    for (const auto &element: tempWordMap) {
+        if (!(element.second % 2)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    string word;
+    cin >> word;
+
+    vector<size_t> subArrays(word.size() + 1, LLONG_MAX);
+    subArrays[0] = 0;
+
+    for (size_t i = 0; i < word.size() - 1; ++i) {
+        if (subArrays[i] != INT_MAX) {
+            wordMap.clear();
+            for (size_t j = i; j < word.size(); ++j) {
+                wordMap[word[j]]++;
+                if (isPossible(wordMap)) {
+                    subArrays[j + 1] = min(subArrays[i] + 1, subArrays[j + 1]);
+                }
+            }
+        }
+    }
+
+    cout << subArrays[word.size()];
+}
