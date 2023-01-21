af9ea02c9e2fef3099e9d7972d8e0e281449fc57
Stoyan Zlatev <47269030+Stoyan-Zlatev@users.noreply.github.com>
2023-01-21 15:26:27 +0200

    Add PE 01 Task 01 Solution

---
+++ Practice/Exams/Exam 01/2019-2020/Task 01/Stoyan.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<size_t, size_t> numberMap;
+const size_t MAX_NUMBER = 99999;
+
+int main() {
+    size_t N;
+    cin >> N;
+
+    size_t number;
+    for (size_t i = 0; i < N; ++i) {
+        cin >> number;
+        numberMap[number]++;
+    }
+
+    for (int i = MAX_NUMBER; i >= 0; --i) {
+        if (numberMap[i]) {
+            for (size_t j = 0; j < numberMap[i]; ++j) {
+                cout << i << " ";
+            }
+        }
+    }
+    
+    return 0;
+}
