5c75713fe59d5d685b4b7e99f10ce763d2231fdc
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2023-05-19 12:56:06 +0300

    Update Georgi.hpp

--- Data Structures/Stack/Georgi.hpp
+++ Data Structures/Stack/Georgi.hpp
@@ -1,144 +1,168 @@
 #pragma once
-#include <iostream>
+#include <exception>
 
 template<typename T>
-struct StackElement
-{
-	T data;
-	StackElement<T>* next;
-};
+class Stack {
+private:
 
-template<typename T>
-class Stack
-{
-	StackElement<T>* top;
+	struct Node {
+		T data;
+		Node* next;
+
+		Node(const T& data, Node* next = nullptr);
+		Node(T&& data, Node* next = nullptr);
+	};
+
+	Node* top = nullptr;
+	size_t size = 0;
+
 public:
-	Stack();
-	Stack(const Stack<T>&);
-	Stack<T>& operator=(const Stack<T>&);
+	Stack() = default;
+	Stack(const Stack<T>& other);
+	Stack(Stack<T>&& other) noexcept;
+	Stack<T>& operator=(const Stack<T>& other);
+	Stack<T>& operator=(Stack<T>&& other) noexcept;
 	~Stack();
 
-	void push(const T&);
-	T pop();
+	void push(const T& data);
+	void push(T&& data);
+	void pop();
 
-	T peek() const;
-	bool empty() const;
+	const T& peek() const;
+	size_t getSize() const;
+	bool isEmpty() const;
+
 private:
-	void copy(const Stack<T>&);
+	void move(Stack<T>&& other) noexcept;
+	void copyFrom(const Stack<T>& other);
 	void free();
 };
 
-template <typename T>
-Stack<T>::Stack()
-{
-	top = nullptr;
+template<typename T>
+Stack<T>::Node::Node(const T& data, Stack<T>::Node* next)
+	: data(data), next(next) { }
+
+template<typename T>
+Stack<T>::Node::Node(T&& data, Stack<T>::Node* next)
+	: data(std::move(data)), next(next) { }
+
+
+template<typename T>
+Stack<T>::Stack(const Stack<T>& other) {
+	copyFrom(other);
 }
 
-template <typename T>
-Stack<T>::Stack(const Stack<T>& other)
-{
-	copy(other);
+template<typename T>
+Stack<T>::Stack(Stack<T>&& other) noexcept {
+	move(std::move(other));
 }
 
-template <typename T>
-Stack<T>& Stack<T>::operator=(const Stack<T>& other)
-{
-	if (this != &other)
-	{
+template<typename T>
+Stack<T>& Stack<T>::operator=(const Stack<T>& other) {
+	if (this != &other) {
 		free();
-		copy(other);
+		copyFrom(other);
 	}
 
 	return *this;
 }
 
-template <typename T>
-Stack<T>::~Stack()
-{
+template<typename T>
+Stack<T>& Stack<T>::operator=(Stack<T>&& other) noexcept {
+	if (this != &other) {
+		free();
+		move(std::move(other));
+	}
+
+	return *this;
+}
+
+template<typename T>
+Stack<T>::~Stack() {
 	free();
 }
 
-template <typename T>
-void Stack<T>::copy(const Stack<T>& s)
-{
-	if (s.top == nullptr)
-	{
-		top = nullptr;
-		return;
-	}
-
-	StackElement<T>* toCopy = nullptr, *newElement = nullptr, *previousElement = nullptr;
-	toCopy = s.top;
-
-	newElement = new StackElement<T>;
-	newElement->data = toCopy->data; //copy
-	newElement->next = nullptr;
-	top = newElement;
-
-	previousElement = top;
-	toCopy = toCopy->next;
-
-	while (toCopy != nullptr)
-	{
-		newElement = new StackElement<T>;
-		newElement->data = toCopy->data; //copy
-		newElement->next = nullptr;
-
-		previousElement->next = newElement;
-		previousElement = newElement;
-		toCopy = toCopy->next;
-	}
-
+template<typename T>
+void Stack<T>::push(const T& data) {
+	top = new Node(data, top);
+	size++;
 }
 
-template <typename T>
-void Stack<T>::free()
-{
-	StackElement<T>* toDelete;
+template<typename T>
+void Stack<T>::push(T&& data) {
+	top = new Node(std::move(data), top);
+	size++;
+}
 
-	while (top != nullptr)
-	{
-		toDelete = top;
-		top = top->next;
-		delete toDelete;
+template<typename T>
+void Stack<T>::pop() {
+	if (isEmpty()) {
+		throw std::exception("Empty collection");
 	}
-}
 
-template <typename T>
-void Stack<T>::push(const T& d)
-{
-	StackElement<T>* p = new StackElement<T>;
-	p->data = d;
-	p->next = top;
-
-	top = p;
-}
-
-template <typename T>
-T Stack<T>::pop()
-{
-	if (empty())
-		throw "Stack is empty!";
-
-	StackElement<T>* p = top;
+	Node* toDelete = top;
 	top = top->next;
-	T data = p->data;
-	delete p;
-
-	return data;
+	delete toDelete;
+	--size;
 }
 
-template <typename T>
-T Stack<T>::peek() const
-{
-	if (empty())
-		throw "Stack is empty";
+template<typename T>
+const T& Stack<T>::peek() const {
+	if (isEmpty()) {
+		throw std::exception("Empty collection");
+	}
 
 	return top->data;
 }
 
-template <typename T>
-bool Stack<T>::empty() const
-{
-	return top == nullptr;
+template<typename T>
+size_t Stack<T>::getSize() const {
+	return size;
+}
+
+template<typename T>
+bool Stack<T>::isEmpty() const {
+	return size == 0;
+}
+
+template<typename T>
+void Stack<T>::move(Stack<T>&& other) noexcept {
+	size = other.size;
+	top = other.top;
+	other.top = nullptr;
+}
+
+template<typename T>
+void Stack<T>::copyFrom(const Stack<T>& other) {
+	if (other.size == 0) {
+		top = nullptr;
+		size = 0;
+		return;
+	}
+
+	size = other.size;
+	Node* toCopyIter = other.top;
+	Node* currentIter = new Node(toCopyIter->data);
+
+	top = currentIter;
+	toCopyIter = toCopyIter->next;
+
+	while (toCopyIter) {
+		currentIter->next = new Node(toCopyIter->data);
+		currentIter = currentIter->next;
+		toCopyIter = toCopyIter->next;
+	}
+}
+
+template<typename T>
+void Stack<T>::free() {
+	Node* toDelete = top;
+
+	while (top) {
+		toDelete = top;
+		top = top->next;
+		delete toDelete;
+	}
+
+	size = 0;
 }
